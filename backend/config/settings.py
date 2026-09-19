8b16a380eea59706eaae77cbad71db2282e592db
Kanishaa Baraneetharan <cbkanishaa@gmail.com>
2026-09-19 15:00:16 +0530

    edited allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-local-secret-key-chang
 
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
-allowed_hosts_env = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1')
+allowed_hosts_env = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1','vastraa-boutique.onrender.com')
 ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_env.split(',') if host.strip()]
 
 # Application definition
